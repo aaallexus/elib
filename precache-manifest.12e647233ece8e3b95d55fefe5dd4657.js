cebac4593d1ea75f79d7551c43cab50e9e9fd0f1
alexei <alexei@localhost.localdomain>
2020-05-05 04:03:20 +0300

    init

---
+++ precache-manifest.12e647233ece8e3b95d55fefe5dd4657.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6214dd6e907bf5f91df9a0a3a8fa294a",
+    "url": "./index.html"
+  },
+  {
+    "revision": "9c922db4f5201bf2388c",
+    "url": "./static/css/main.152f417b.chunk.css"
+  },
+  {
+    "revision": "ad5d057a8c86318e26f0",
+    "url": "./static/js/2.e2d03da3.chunk.js"
+  },
+  {
+    "revision": "c90b5eae1b0eb71148dd6638cdd8b617",
+    "url": "./static/js/2.e2d03da3.chunk.js.LICENSE"
+  },
+  {
+    "revision": "9c922db4f5201bf2388c",
+    "url": "./static/js/main.864a6238.chunk.js"
+  },
+  {
+    "revision": "b5363620ffc3bea02f86",
+    "url": "./static/js/runtime-main.927be584.js"
+  }
+]);
